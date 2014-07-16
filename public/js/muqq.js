bbde32492c244297a8de5aabb9ace8bfceb53f73
Muqq <bbbb55952000@gmail.com>
2014-07-16 08:59:10 +0800

    about page

--- public/js/muqq.js
+++ public/js/muqq.js
@@ -4,85 +4,93 @@ var opApp = angular.module('opApp', [
     'angularFileUpload',
     '$strap.directives',
     'opControllers'
-], function($locationProvider) {
-    $locationProvider.html5Mode(true);
-});
+    ], function($locationProvider) {
+        $locationProvider.html5Mode(true);
+    });
 
 var opControllers = angular.module('opControllers', []);
 
 
 opControllers.controller('op-home-control', ['$scope', '$http', '$window',
     function($scope, $http, $window) {
-        var arrowCount = 0 ;
-        setInterval(function(){
-            if (arrowCount%2==0){
-                $('#arrow').removeClass('down');
-            }else{
-                $('#arrow').addClass('down');
-            }
-            arrowCount += 1 ;
-        },500);
-        function changeText(cont1,cont2,speed){
-            var Otext= "Raymn Agency , a team consist of vitality , creativity and dream";
-            var Otext2 = "We design visual , build connection , we narrate brand story.";
-            var Ocontent=Otext.split("");
-            var Ocontent2 = Otext2.split("");
-            var interval ;
-            var counter = 0 ;
-            function showLine(textArray, htmlText, htmlClass){
-                if (counter<textArray.length){
-                    htmlText.append(textArray[counter]);
-                    counter += 1 ;
-                }else{
-                    clearInterval(interval);
-                    setTimeout(function(){
-                        $(htmlClass).html("");
-                        if (counter%2 ===0){
-                            start(function(){showLine(Ocontent2, cont2, ".p2")});
-                        }else{
-                            start(function(){showLine(Ocontent, cont1, ".p1")});
-                        }
-                        counter = 0 ;
-                    },2000);
-                }
-            }
-            function start(line) {
-                interval = setInterval(line,speed);
-            }
-            start(function(){showLine(Ocontent, cont1, ".p1")});
-        }
-
         $(document).ready(function() {
+            for (var i = 1 ; i<6 ; i++){
+                $('#card' + i).css({bottom:-200*i, opacity:0});
+            } 
             $('#fullpage').fullpage({
-                 anchors: ['home','aboutPage','servicesPage','newsPage','workPage','contactPage'],
+                anchors: ['Page0', 'Page1', 'Page2', 'Page3', 'Page4', 'Page5'],
                 afterLoad: function(anchorLink, index){
-                //using index
-                    anchorLink = anchorLink.replace('Page','');
-                    var array = ['about' , 'services','news','work','contact'];
-                    var idx = array.indexOf(anchorLink);
-                    array.splice(idx,1);
-                    console.log(array);
-                    $('#'+anchorLink).attr("src", "/img/LoginView/"+anchorLink+"_0.png");
-                    for (var i=0 ; i<array.length ; i++){
-                        $('#'+array[i]).attr("src", "/img/LoginView/"+array[i]+"_1.png");
-                    }
+                    $scope.changeColor(index-1);
                 },
                 onLeave: function(index, direction){
                     console.log(index, direction);
-                     if (direction==2){
+                    if (direction==2){
+                        setTimeout(function(){
+                            for (var i = 1 ; i<6 ; i++){
+                                $('#card' + i).css({bottom:'0px', opacity:1});
+                            }
+                        },500);
+
+                    }else if ((index>direction) && (direction!=2)){
                         for (var i = 1 ; i<6 ; i++){
-                            $('#card'+i).removeClass('out-of-edgecard'+i);
-                        }
-                    }else{
+                            $('#card' + i).css({bottom:-200*i, opacity:0});
+                        }                        
+                    }else if ((index<direction) && (direction!=2)){
                         for (var i = 1 ; i<6 ; i++){
-                            $('#card'+i).addClass('out-of-edgecard'+i);
-                        }
+                            $('#card' + i).css({bottom:+200*i, opacity:0});
+                        } 
                     }
                     
                 }
             });
-            changeText($(".p1"),$(".p2"),50);
-        });
+
+});
+
+function cardExplode()  {
+    var ran = new Array();
+    for (var i = 0; i<11; i++) {
+        ran[i] = (Math.random()-0.5)*2000;
+    }
+    for (var j = 1; j<6; j++) {
+        $('#card'+j).attr('id', 'card' + j + 'WhenClick');
+        $('#card' + j + 'WhenClick').css({bottom: ran[j]+'px', left: ran[j+5]+'px', visibility: 'hidden'});
+    }
+}
+function cardExplodeRe() {
+    for (var i = 1; i < 6; i++) {
+        $('#card' + i + 'WhenClick').css({bottom: 0+'px', left: 0+'px', visibility: 'visible'});
+        $('#card' + i + 'WhenClick').attr('id','card' + i);
+    }
+}     
+function changeText(cont1,cont2,speed){
+    var Otext= "Raymn Agency , a team consist of vitality , creativity and dream";
+    var Otext2 = "We design visual , build connection , we narrate brand story.";
+    var Ocontent=Otext.split("");
+    var Ocontent2 = Otext2.split("");
+    var interval ;
+    var counter = 0 ;
+    function showLine(textArray, htmlText, htmlClass){
+        if (counter<textArray.length){
+            htmlText.append(textArray[counter]);
+            counter += 1 ;
+        }else{
+            clearInterval(interval);
+            setTimeout(function(){
+                $(htmlClass).html("");
+                if (counter%2 ===0){
+                    start(function(){showLine(Ocontent2, cont2, "#p2")});
+                }else{
+                    start(function(){showLine(Ocontent, cont1, "#p1")});
+                }
+                counter = 0 ;
+            },2000);
+        }
+    }
+    function start(line) {
+        interval = setInterval(line,speed);
+    }
+    start(function(){showLine(Ocontent, cont1, "#p1")});
+}
         // init size
         //i love fifi's mother
         var rate = $window.innerWidth/1458 ;
@@ -104,133 +112,113 @@ opControllers.controller('op-home-control', ['$scope', '$http', '$window',
         $(window).resize(function(){
             $scope.$apply(function(){
             //do something to update current scope based on the new innerWidth and let angular update the view.
-                size();
-            });
+            size();
         });
-        //pictucre slider
- 
-   
-
-        $scope.buttonChange = function(name){
-            var query = '#'+name ;
-            $(query).mouseenter(function() {
-                $(query).attr("src", "/img/LoginView/"+name+"_0.png");
-            }).mouseleave(function() {
-                if (!$(this).hasClass('clicked')){
-                    console.log('cancel');
-                    $(query).attr("src", "/img/LoginView/"+name+"_1.png");
-                }
-            }).click(function(){
-                $(this).addClass('clicked');
-                var array = ['about' , 'services','news','work','contact'];
-                var idx = array.indexOf(name);
-                array.splice(idx,1);
-                $(query).attr("src", "/img/LoginView/"+name+"_0.png");
-                for (var i=0 ; i<array.length ; i++){
-                    $('#'+array[i]).attr("src", "/img/LoginView/"+array[i]+"_1.png");
-                    if($('#'+array[i]).hasClass('clicked'))
-                        $('#'+array[i]).removeClass('clicked');
-                }
-            });
-        }
-
-        $scope.homeButton = function(){
-            var array = ['about' , 'services','news','work','contact'];
-            for (var i=0 ; i<array.length ; i++){
-                    $('#'+array[i]).attr("src", "/img/LoginView/"+array[i]+"_1.png");
-                    if($('#'+array[i]).hasClass('clicked'))
-                        $('#'+array[i]).removeClass('clicked');
+        });
+        $scope.changeColor = function(textNumber){
+            for (var i=1; i<6 ; i++){
+                $('#text'+i).attr('class', 'rightWord');
             }
-            
+            if(textNumber != 0){
+                $('#text'+textNumber).attr('class', 'rightWord2');
+            }else{
+
+            }
         }
         
-        // $scope.showCard = function(id){
-        //     var textId = id ;
-        //     textId = textId.replace('card', 'photo');
-        //     $scope.show = true ; 
-        //     //$('#'+textId).css({display:'block'});
-        // }
-
-        // $scope.hideCard = function(id){
-        //     var textId = id ;
-        //     textId = textId.replace('card', 'photo');
-        //     $scope.show = false ;
-        //     //$('#'+textId).css({display:'none'});
-        // }
-
-
-
-        $scope.showContent = function(id){
-            $('#section1_2').show();
-            $('#section1_2_1').show();
-            var textId = id ;
-            textId = textId.replace('card','');
-            $scope.content = $scope.data[textId-1];
+        $scope.clickCard = function(cardNumber){
+            if($('#photoImg'+cardNumber).css('opacity')!=0){
+                $('#section1_2').css('visibility','visible');
+                $scope.content = $scope.data[cardNumber-1];
+                cardExplode();   
+            }
         }
 
-        $scope.hideContent = function(){
-            $('#section1_2').hide();
-            $('#section1_2_1').hide();
+        $scope.clickDark = function(){
+            $('#section1_2').css('visibility','hidden');
+            cardExplodeRe();
         }
+
+        $scope.photoImgShow = function(cardNumber){
+            $('#nameImgHover'+cardNumber).attr('href','css/cardHover/card'+cardNumber+'Hover.css');
+            $('#photoImg'+cardNumber).css({cursor:'pointer'});
+        }
+        $scope.photoImgHide = function(cardNumber){
+            console.log('leave');
+            $('#nameImgHover'+cardNumber).attr('href','');
+            $('#photoImg'+cardNumber).css({cursor:''});
+        }
+        $scope.photoImgShowAgain = function(cardNumber){
+            if($('#photoImg'+cardNumber).css('opacity')!=0){
+                $scope.photoImgShow(cardNumber);
+            }else{
+
+            }
+        }
+
 
         $(document).keyup(function(e) {
             if (e.keyCode == 27) { 
-                $('#section1_2').hide();
-                $('#section1_2_1').hide(); 
+                $scope.clickDark();
             }   // esc
         });
 
         $scope.data = [
-            {
-                id: 'card1',
-                nameImg: 'img/About/ms.png',
-                img: 'img/About/photo_1.jpg',
-                allName:'Muqq Shih',
-                title:'CEO',
-                photoId :'photo1',
-                p1:'話說天下大勢．分久必合，合久必分：周末七國分爭，并入於秦。及秦滅之後，楚、漢分爭，又并入於漢。漢朝自高祖斬白蛇而起義，一統天下。後來光武中興，傳至獻帝，遂分為三國。'
-            },
-            {
-                id: 'card2',
-                nameImg: 'img/About/sk.png',
-                img: 'img/About/photo_2.jpg',
-                allName:'Sprout Kuo',
-                title:'CTO',
-                photoId :'photo2',
-                p1:'話說天下大勢．分久必合，合久必分：周末七國分爭，并入於秦。及秦滅之後，楚、漢分爭，又并入於漢。漢朝自高祖斬白蛇而起義，一統天下。後來光武中興，傳至獻帝，遂分為三國。'          
-            },
-            {
-                id: 'card3',
-                nameImg: 'img/About/yk.png',
-                img: 'img/About/photo_3.jpg',
-                allName:'Youlanda Kuo',
-                title:'Desinger',
-                photoId :'photo3',
-                p1:'話說天下大勢．分久必合，合久必分：周末七國分爭，并入於秦。及秦滅之後，楚、漢分爭，又并入於漢。漢朝自高祖斬白蛇而起義，一統天下。後來光武中興，傳至獻帝，遂分為三國。'
-            },
-            {
-                id: 'card4',
-                nameImg: 'img/About/sf.png',
-                img: 'img/About/photo_4.jpg',
-                allName:'Shaofu',
-                title:'Manager',
-                photoId :'photo4',
-                p1:'話說天下大勢．分久必合，合久必分：周末七國分爭，并入於秦。及秦滅之後，楚、漢分爭，又并入於漢。漢朝自高祖斬白蛇而起義，一統天下。後來光武中興，傳至獻帝，遂分為三國。'  
-            },
-            {
-                id: 'card5',
-                nameImg: 'img/About/nn.png',
-                img: 'img/About/photo_5.jpg',
-                allName:'Niao Niao',
-                title:'Mascot',
-                photoId :'photo5',
-                p1:'話說天下大勢．分久必合，合久必分：周末七國分爭，并入於秦。及秦滅之後，楚、漢分爭，又并入於漢。漢朝自高祖斬白蛇而起義，一統天下。後來光武中興，傳至獻帝，遂分為三國。'
-            }
+        {
+            number : 1,
+            id: 'card1',
+            nameImg: 'img/About/ms.png',
+            img: 'img/About/photo_1.jpg',
+            allName:'Muqq Shih',
+            title:'CEO',
+            photoId :'photo1',
+            p1:'話說天下大勢．分久必合，合久必分：周末七國分爭，并入於秦。及秦滅之後，楚、漢分爭，又并入於漢。漢朝自高祖斬白蛇而起義，一統天下。後來光武中興，傳至獻帝，遂分為三國。'
+        },
+        {
+            number : 2,
+            id: 'card2',
+            nameImg: 'img/About/sk.png',
+            img: 'img/About/photo_2.jpg',
+            allName:'Sprout Kuo',
+            title:'CTO',
+            photoId :'photo2',
+            p1:'話說天下大勢．分久必合，合久必分：周末七國分爭，并入於秦。及秦滅之後，楚、漢分爭，又并入於漢。漢朝自高祖斬白蛇而起義，一統天下。後來光武中興，傳至獻帝，遂分為三國。'          
+        },
+        {
+            number : 3 ,
+            id: 'card3',
+            nameImg: 'img/About/yk.png',
+            img: 'img/About/photo_3.jpg',
+            allName:'Youlanda Kuo',
+            title:'Desinger',
+            photoId :'photo3',
+            p1:'話說天下大勢．分久必合，合久必分：周末七國分爭，并入於秦。及秦滅之後，楚、漢分爭，又并入於漢。漢朝自高祖斬白蛇而起義，一統天下。後來光武中興，傳至獻帝，遂分為三國。'
+        },
+        {
+            number : 4 ,
+            id: 'card4',
+            nameImg: 'img/About/sf.png',
+            img: 'img/About/photo_4.jpg',
+            allName:'Shaofu',
+            title:'Manager',
+            photoId :'photo4',
+            p1:'話說天下大勢．分久必合，合久必分：周末七國分爭，并入於秦。及秦滅之後，楚、漢分爭，又并入於漢。漢朝自高祖斬白蛇而起義，一統天下。後來光武中興，傳至獻帝，遂分為三國。'  
+        },
+        {
+            number : 5 ,
+            id: 'card5',
+            nameImg: 'img/About/nn.png',
+            img: 'img/About/photo_5.jpg',
+            allName:'Niao Niao',
+            title:'Mascot',
+            photoId :'photo5',
+            p1:'話說天下大勢．分久必合，合久必分：周末七國分爭，并入於秦。及秦滅之後，楚、漢分爭，又并入於漢。漢朝自高祖斬白蛇而起義，一統天下。後來光武中興，傳至獻帝，遂分為三國。'
+        }
         ]
 
         size();
-        
+        changeText($("#p1"),$("#p2"),50);
 
 
-     }
-]);
+    }
+    ]);
